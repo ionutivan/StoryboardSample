a2eca57071d4fccf4277e0f4e4066f7aff57de58
Ionut Ivan <ii@goldengekko.com>
2014-04-23 17:07:19 +0200

    first commit

---
+++ StoryboardMerge/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  StoryboardMerge
+//
+//  Created by Ionut Ivan on 23/04/14.
+//  Copyright (c) 2014 Ionut Ivan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ StoryboardMerge/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  StoryboardMerge
+//
+//  Created by Ionut Ivan on 23/04/14.
+//  Copyright (c) 2014 Ionut Ivan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
